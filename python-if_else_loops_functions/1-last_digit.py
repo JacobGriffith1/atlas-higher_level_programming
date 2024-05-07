e1621c7a3a78254d25457800f79f2c65d26b7b74
JacobGriffith1 <jacobgriffith6339@gmail.com>
2024-05-06 19:09:59 -0500

    1-last_digit.py [2]: Fix to account for the checker's bad usage of the word "digit"

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
+i = 10
 print(f"Last digit of {number}", end=' ')
 if number < 0:
-    number = number * -1
-print(f"is {number % 10}", end=' ')
-if number % 10 > 5:
+    i = -10
+print(f"is {number % i}", end=' ')
+if number % i > 5:
     print(f"and is greater than 5")
-elif number % 10 == 0:
+elif number % i == 0:
     print(f"and is 0")
 else:
     print(f"is less than 6 and not 0")
