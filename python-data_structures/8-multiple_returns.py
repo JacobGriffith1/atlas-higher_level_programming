6fceac61a4ba5e903303a010d90621bf82d69191
JacobGriffith1 <jacobgriffith6339@gmail.com>
2024-05-08 10:44:25 -0500

    8-multiple_returns.py [2]

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -5,6 +5,6 @@ def multiple_returns(sentence):
     if (length == 0):
         new_tuple = (length, None)
     else:
-        new_tuple = (length, sentence)
+        new_tuple = (length, sentence[0])
 
     return (new_tuple)
