65326be67b5a149130d442aedc3f7d85ad59a56f
JacobGriffith1 <jacobgriffith6339@gmail.com>
2024-05-08 00:08:21 -0500

    4-new_in_list.py [1]

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -1,2 +1,11 @@
 #!/usr/bin/python3
+def new_in_list(my_list, idx, element):
+    length = len(my_list)
 
+    new_list = my_list[:]
+    # [:] will cut out all elements of my_list without changing the len
+
+    if 0 <= idx < length:
+        new_list[idx] = element
+
+    return (new_in_list)
