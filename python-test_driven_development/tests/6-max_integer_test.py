a37b985344fc6a6e3067512150e5b156a06cfa45
JacobGriffith1 <jacobgriffith6339@gmail.com>
2024-05-15 14:57:32 -0500

    6-max_integer_test.py [2]

--- python-test_driven_development/tests/6-max_integer_test.py
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -3,4 +3,31 @@ import unittest
 max_integer = __import__('6-max_integer').max_integer
 
 class TestMaxInteger(unittest.TestCase):
-    def 
+    def maxLast(self):
+        """max at the end"""
+        mlast = [1, 2, 3, 4]
+        self.assertEqual(max_integer(mlast), 4)
+    def maxFirst(self):
+        """max at the beginning"""
+        mfirst = [4, 1, 2, 3]
+        self.assertEqual(max_integer(mfirst), 4)
+    def maxMid(self):
+        """max in the middle"""
+        mmid = [1, 2, 5, 3, 4]
+        self.assertEqual(max_integer(mmid), 5)
+    def oneNeg(self):
+        """one negative number in the list"""
+        oneg = [1, 2, 3, 4, -5]
+        self.assertEqual(max_integer(oneg), 4)
+    def allNeg(self):
+        """only negative numbers in the list"""
+        aneg = [-4, -3, -2, -1]
+        self.assertEqual(max_integer(aneg), -1)
+    def oneElem(self):
+        """list of one element"""
+        one = [1]
+        self.assertEqual(max_integer(one), 1)
+    def emptyList(self):
+        """list is empty"""
+        empty = []
+        self.assertEqual(max_integer(empty), None)
