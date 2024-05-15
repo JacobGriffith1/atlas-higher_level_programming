0366b7173750b91bdab26a09e08530a43b270826
JacobGriffith1 <jacobgriffith6339@gmail.com>
2024-05-14 23:11:00 -0500

    4-print_square.py [1]

--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -1,1 +1,26 @@
 #!/usr/bin/python3
+"""
+Module contains function that prints a square of '#'
+"""
+
+
+def print_square(size):
+    """Function prints a square of '#'
+    
+    Args:
+        size: size of the square to print
+
+    Return:
+        N/A
+    
+    Raises:
+        TypeError: Size is not an int
+    """
+
+    if not isinstance(size, int):
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be  >= 0")
+    
+    for i in range(size):
+        print("#" * size)
