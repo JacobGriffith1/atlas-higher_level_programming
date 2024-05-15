7ab0ca24d78bc0dfe10d9af0d283235c0d3f9905
JacobGriffith1 <jacobgriffith6339@gmail.com>
2024-05-15 00:22:52 -0500

    0-square_matrix_simple.py [1]

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -1,2 +1,8 @@
 #!/usr/bin/python3
+def square_matrix_simple(matrix=[]):
+    newMat = matrix.copy()
 
+    for i in range(len(matrix)):
+        newMat[i] = list(map(lambda x: x**2, matrix[i]))
+
+    return (newMat)
