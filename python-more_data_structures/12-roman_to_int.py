c458a52f66494755f41da8bed6ed8d2faa1e4052
JacobGriffith1 <jacobgriffith6339@gmail.com>
2024-05-15 01:32:13 -0500

    12-roman_to_int.py [4]: pycode fixing; extra line added between funcs, white space fixing

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -6,15 +6,16 @@ def subtract(numList):
     for n in numList:
         if maxList > n:
             sub += n
-    
+
     return (maxList - sub)
+
 
 def roman_to_int(roman_string):
     if not roman_string:
         return (0)
     if not isinstance(roman_string, str):
         return (0)
-    
+
     romNum = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     keyList = list(romNum.keys())
 
