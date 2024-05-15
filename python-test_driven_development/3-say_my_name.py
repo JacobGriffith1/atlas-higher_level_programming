9f5d0a636609f89fe248903522e3993f9f7caa59
JacobGriffith1 <jacobgriffith6339@gmail.com>
2024-05-14 22:43:16 -0500

    3-say_my_name.py [2]: Indents fix

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -18,10 +18,10 @@ def say_my_name(first_name, last_name=""):
         TypeError: Either variable is not a string
     """
 
-if type(first_name) is not str:
-    raise TypeError("first_name must be a string")
+    if type(first_name) is not str:
+        raise TypeError("first_name must be a string")
 
-if type(last_name) is not str:
-    raise TypeError("last_name must be a string")
+    if type(last_name) is not str:
+        raise TypeError("last_name must be a string")
 
-print("My name is {} {}".format(first_name, lastname))
+    print("My name is {} {}".format(first_name, last_name))
