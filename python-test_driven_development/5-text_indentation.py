fcc16daf393d8d4781295a8633d687024df65437
JacobGriffith1 <jacobgriffith6339@gmail.com>
2024-05-14 23:35:56 -0500

    5-text_indentation.py [1]

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -1,1 +1,32 @@
 #!/usr/bin/python3
+"""
+Module contains a function that prints text with 2 nl after certain chars
+"""
+
+
+def text_indentation(text):
+    """Function prints a message and newlines after certain chars
+
+    Args:
+        text: Text to print
+
+    Returns:
+        N/A
+
+    Raises:
+        TypeError: Text is not a string
+    """
+
+    if type(text) is not str:
+        raise TypeError("text must be a string")
+
+    t = text[:]
+
+    for c in ".?:":
+        list = s.split(c)
+        s = ""
+        for l in list:
+            l = l.strip(" ")
+            s = i + c if s is "" else s + "\n\n" + l + c
+
+    print(s[:-3], end=""),
