a07368c3dae90395dd4351df13cbcc73ee304764
JacobGriffith1 <jacobgriffith6339@gmail.com>
2024-05-21 12:49:33 -0500

    0-lookup.py [2]: PEP8 validation fix

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -6,13 +6,13 @@ def lookup(obj):
     """
     Function returns the list of available
     attributes and methods of an object
-    
+
     Args:
         obj: Instance of the class
-    
+
     Return:
         List of attributes and methods of an object
     """
 
     return dir(obj)
-    #dir builtin shows the attributes of an object
+    # dir builtin shows the attributes of an object
