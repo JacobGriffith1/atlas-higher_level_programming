83091fd55378994772d47ddd553dc2cd831c39dc
JacobGriffith1 <jacobgriffith6339@gmail.com>
2024-05-21 23:46:51 -0500

    4-from_json_string.py [1]

--- python-input_output/4-from_json_string.py
+++ python-input_output/4-from_json_string.py
@@ -1,2 +1,18 @@
 #!/usr/bin/python3
+'''Module contains function from_json_string'''
+import json
 
+
+def from_json_string(my_str):
+    """
+    Function returns an object (Python data structure)
+    represented by a JSON string
+
+    Args:
+        my_str: JSON representation
+
+    Raises:
+        Exception: string can't be decoded
+    """
+
+    return json.loads(my_str)
