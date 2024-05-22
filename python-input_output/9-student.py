3d4a38b4025047cbc7df603a5f25b3a43e3cb6c3
JacobGriffith1 <jacobgriffith6339@gmail.com>
2024-05-22 13:42:31 -0500

    9-student.py [1]

--- python-input_output/9-student.py
+++ python-input_output/9-student.py
@@ -1,2 +1,16 @@
 #!/usr/bin/python3
+'''Module contains class Student'''
 
+
+class Student:
+    """Creates student instances"""
+
+    def __init__(self, first_name, last_name, age):
+        """Initialise instance"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """Returns dict description"""
+        return self.__dict__.copy()
