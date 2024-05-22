3756694682bc5287b6a75b5695969febc6efdcf2
JacobGriffith1 <jacobgriffith6339@gmail.com>
2024-05-21 23:29:55 -0500

    0-read_file.py [2]: indent line 16, 17, 18

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -5,14 +5,14 @@
 def read_file(filename=""):
     """
     Function reads text file (UTF8) and prints it to stdout
-    
+
     Args:
         filename: file name
-    
+
     Raises:
         Exception: file can be opened
     """
 
-with open(filename, 'r', encoding="utf-8") as f:
-    data = f.read()
-    print(data, end='')
+    with open(filename, 'r', encoding="utf-8") as f:
+        data = f.read()
+        print(data, end='')
