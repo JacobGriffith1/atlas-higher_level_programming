ed58661bbf4e417ced11d1eb7faf63373aaa5126
JacobGriffith1 <jacobgriffith6339@gmail.com>
2024-05-21 23:28:04 -0500

    0-read_file.py

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,2 +1,18 @@
 #!/usr/bin/python3
+'''Module contains function read_file'''
 
+
+def read_file(filename=""):
+    """
+    Function reads text file (UTF8) and prints it to stdout
+    
+    Args:
+        filename: file name
+    
+    Raises:
+        Exception: file can be opened
+    """
+
+with open(filename, 'r', encoding="utf-8") as f:
+    data = f.read()
+    print(data, end='')
