e55ffa655022c2062f467d8ca992b16ee28f2bf9
JacobGriffith1 <jacobgriffith6339@gmail.com>
2024-05-21 23:08:35 -0500

    10-square.py [1]

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -1,2 +1,16 @@
 #!/usr/bin/python3
+'''Module contains class Square that inherits from Rectangle'''
+Rectangle = __import__('9-rectangle').Rectangle
 
+
+class Square(Rectangle):
+    """Class defines a Square from Rectangle"""
+    def __init__(self, size):
+        """Initializes square"""
+        self.integer_validator("size", size)
+        self.__size = size
+        super().__init__(self.__size, self.__size)
+
+    def area(self):
+        """Returns area"""
+        return super().area()# super() returns a proxy object representing the parent's class
