75de56cd2b462c7292552065cdffb527a4a3012c
JacobGriffith1 <jacobgriffith6339@gmail.com>
2024-05-21 23:56:37 -0500

    6-load_from_json_file.py [1]

--- python-input_output/6-load_from_json_file.py
+++ python-input_output/6-load_from_json_file.py
@@ -1,2 +1,18 @@
 #!/usr/bin/python3
+'''Module contains function load_from_json_file'''
+import json
 
+
+def load_from_json_file(filename):
+    """
+    Function creates an Object from a "JSON file"
+
+    Args:
+        filename: textfile name
+
+    Raises:
+        Exception: object can't be encoded
+    """
+
+    with open(filename, 'r', encoding="utf-8") as f:
+        return json.load(f)
