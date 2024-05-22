182c4ffa97d21d922a1b41f3420eb435cde2ce1f
JacobGriffith1 <jacobgriffith6339@gmail.com>
2024-05-22 00:09:02 -0500

    7-add_item.py [3]: forgot to add the function to lines 8 & 9, functions appended

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -5,8 +5,8 @@ import sys
 import os.path
 
 
-save = __import__('5-save_to_json_file')
-load = __import__('6-load_from_json_file')
+save = __import__('5-save_to_json_file').save_to_json_file
+load = __import__('6-load_from_json_file').load_from_json_file
 
 list = []
 if os.path.exists("add_item.json"):
