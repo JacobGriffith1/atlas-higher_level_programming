fee930d6dbbdc2f0d45fc52a188f35e1573d47f5
JacobGriffith1 <jacobgriffith6339@gmail.com>
2024-05-26 17:11:48 -0500

    base.py, test_base.py [1, 1]

--- python-almost_a_circle/tests/test_models/test_base.py
+++ python-almost_a_circle/tests/test_models/test_base.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+"""Tests module for class: Base"""
+import unittest
+from models.base import Base
+from models.square import Square
+from models.rectangle import Rectangle
+from io import StringIO
+from unittest import TestCase
+from unittest.mock import patch
+
+
+class TestBase(unittest.TestCase):
+    """Testing suite for class: Base"""
