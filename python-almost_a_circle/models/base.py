1235ffae19c57260e0fce600e5ac86fbe38385a1
JacobGriffith1 <jacobgriffith6339@gmail.com>
2024-05-26 17:38:55 -0500

    rectangle.py, test_rectangle.py [1, 1]

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -6,7 +6,7 @@ import os.path
 
 
 class Base:
-    """Class Base"""
+    """Class: Base"""
     __nb_objects = 0
 
     def __init__(self, id=None):
--- python-almost_a_circle/tests/test_models/test_rectangle.py
+++ python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python3
+"""Tests module for class: Rectangle"""
+import unittest
+from io import StringIO
+from unittest import TestCase
+from unittest.mock import patch
+from models.rectangle import Rectangle
+from models.base import Base
+
+
+class TestRectangle(unittest.TestCase):
+    """Testing suite for class: Rectangle"""
