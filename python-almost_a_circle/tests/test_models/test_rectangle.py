9c4f4b3a6e48b7b413a5576282704acd5e6ffc6f
JacobGriffith1 <jacobgriffith6339@gmail.com>
2024-05-27 15:48:51 -0500

    test_rectangle.py: isRec, empty; width: priv, getter, setter

--- python-almost_a_circle/tests/test_models/test_rectangle.py
+++ python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -7,3 +7,23 @@ from models.rectangle import Rectangle
 
 class TestRectangle(unittest.TestCase):
     """Testing suite for class: Rectangle"""
+
+    def test_isRec(self):
+        self.assertIsInstance(Rectangle(3, 5), Base)
+
+    def test_empty(self):
+        with self.assertRaises(TypeError):
+            Rectangle()
+
+    def test_width_priv(self):
+        with self.assertRaises(AttributeError):
+            print(Rectangle(2, 4, 6, 8, 10).__width)
+    
+    def test_width_getter(self):
+        rec = Rectangle(2, 4, 6, 8, 10)
+        self.assertEqual(2, rec.width)
+
+    def test_width_setter(self):
+        rec = Rectangle(2, 4, 6, 8, 10)
+        rec.width = 2
+        self.assertEqual(2, rec.width)
