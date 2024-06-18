edcdd81b912a8bd90b3cda9ec9a85c9e48a61666
JacobGriffith1 <jacobgriffith6339@gmail.com>
2024-06-18 10:24:09 -0500

    8-cities [3]: Removed commas from WHERE. Also, FROM and WHERE are now capitalized

--- SQL_more_queries/8-cities_of_california_subquery.sql
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,7 +1,7 @@
 -- Write a script that lists all the cities of California that can be found in the database hbtn_0d_usa.
 SELECT id, name
-from cities
-where state_id = (
-    SELECT id,
-    FROM states,
+FROM cities
+WHERE state_id = (
+    SELECT id
+    FROM states
     WHERE name = "California");
