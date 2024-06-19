fc5002900059e4706bf8a0fdb74dbec7c778a30f
JacobGriffith1 <jacobgriffith6339@gmail.com>
2024-06-19 02:01:52 -0500

    4-cities [1]

--- python-object_relational_mapping/4-cities_by_state.py
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -1,2 +1,23 @@
 #!/usr/bin/python3
+"""
+Script lists all [cities] from the database [hbtn_0e_4_usa]
+"""
+import sys
+import MySQLdb
 
+
+if __name__ == '__main__':
+    db = MySQLdb.connect(user=sys.argv[1],
+                         passwd=sys.argv[2],
+                         db=sys.argv[3],
+                         port=3306)
+
+    csr = db.cursor()
+    csr.execute("SELECT cities.id, cities.name, states.name \
+                 FROM cities \
+                 JOIN states \
+                 ON cities.state_id = states.id;")
+    states = csr.fetchall()
+
+    for state in states:
+        print(state)
