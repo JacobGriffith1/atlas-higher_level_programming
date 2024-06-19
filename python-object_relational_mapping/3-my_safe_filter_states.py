3164b09477329db3fa2904301c9a4e6454ce8008
JacobGriffith1 <jacobgriffith6339@gmail.com>
2024-06-19 01:54:54 -0500

    3-my_safe [1]

--- python-object_relational_mapping/3-my_safe_filter_states.py
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,2 +1,25 @@
 #!/usr/bin/python3
+"""
+Script takes in an argument and displays all values
+in the [states] table of [hbtn_0e_0_usa] where [name]
+matches the argument
+"""
+import sys
+import MySQLdb
 
+
+if __name__ == '__main__':
+    db = MySQLdb.connect(user=sys.argv[1],
+                         passwd=sys.argv[2],
+                         db=sys.argv[3],
+                         port=3306)
+
+    csr = db.cursor()
+    csr.execute("SELECT * \
+                 FROM states \
+                 WHERE name = %s;"
+                 (sys.argv[4],))
+    states = csr.fetchall()
+
+    for state in states:
+        print(state)
