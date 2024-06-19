6083c597bfe59bc35c5814deb37b3b4026e3582e
JacobGriffith1 <jacobgriffith6339@gmail.com>
2024-06-18 23:22:34 -0500

    0-select_states.py [2]: pycode

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -7,11 +7,11 @@ import MySQLdb
 
 
 if __name__ == '__main__':
-    db =MySQLdb.connect(user=sys.argv[1],
+    db = MySQLdb.connect(user=sys.argv[1],
                         passwd=sys.argv[2],
                         db=sys.argv[3],
                         port=3306)
-    
+
     csr = db.cursor()
     csr.execute("SELECT * FROM states;")
     states = csr.fetchall()
