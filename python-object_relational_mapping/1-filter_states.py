1a64ef3679f193ddfaf87be06630c3b1e206da63
JacobGriffith1 <jacobgriffith6339@gmail.com>
2024-06-19 00:03:08 -0500

    1-filter_states.py [1]

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -1,2 +1,25 @@
 #!/usr/bin/python3
+"""
+Script lists all [states] with a [name] starting with [N]
+from database [hbtn_0e_0_usa]
+"""
+import sys
+import MySQLdb
 
+
+if __name__ == '__main__':
+    db = MySQLdb.connect(user=sys.argv[1],
+                         passwd=sys.argv[2],
+                         db=sys.argv[3],
+                         port=3306)
+
+    csr = db.cursor()
+    csr.execute("SELECT * \
+                 FROM `states` \
+                 WHERE `name` \
+                 LIKE 'N%' \
+                 ORDER BY `id`")
+    states = csr.fetchall()
+
+    for state in states:
+        print(state)
