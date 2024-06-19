6760ebc5574007aec9ff6d30f74f4aa20d9b8919
JacobGriffith1 <jacobgriffith6339@gmail.com>
2024-06-19 02:28:04 -0500

    model_state [1]

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -1,2 +1,22 @@
 #!/usr/bin/python3
+"""
+File contains the class definition of a [State] and
+an instance [Base = declarative_base()]
+"""
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
 
+Base = declarative_base
+
+
+class State(Base):
+    """
+    Class contains [id] and [name] attributes of each [State]
+    """
+    __tablename__ = 'states'
+    id = Column(Integer,
+                unique=True,
+                nullable=False,
+                primary_key=True)
+    name = Column(String(128),
+                  nullable=False)
