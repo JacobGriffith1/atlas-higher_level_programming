302522b9baf092a5be15f201d61b6dc3f60ec7dc
JacobGriffith1 <jacobgriffith6339@gmail.com>
2024-06-19 00:11:43 -0500

    1-filter_states.py [2]: LIKE BINARY

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -17,7 +17,7 @@ if __name__ == '__main__':
     csr.execute("SELECT * \
                  FROM `states` \
                  WHERE `name` \
-                 LIKE 'N%' \
+                 LIKE BINARY 'N%' \
                  ORDER BY `id`")
     states = csr.fetchall()
 
