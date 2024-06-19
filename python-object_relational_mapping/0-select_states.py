153374cc80b6bca1cefa86fdf2e31af4e7518574
JacobGriffith1 <jacobgriffith6339@gmail.com>
2024-06-18 23:20:51 -0500

    0-select_states.py [1]

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,2 +1,20 @@
 #!/usr/bin/python3
+"""
+Script lists all [states] from the database [hbtn_0e_0_usa]
+"""
+import sys
+import MySQLdb
 
+
+if __name__ == '__main__':
+    db =MySQLdb.connect(user=sys.argv[1],
+                        passwd=sys.argv[2],
+                        db=sys.argv[3],
+                        port=3306)
+    
+    csr = db.cursor()
+    csr.execute("SELECT * FROM states;")
+    states = csr.fetchall()
+
+    for state in states:
+        print(state)
