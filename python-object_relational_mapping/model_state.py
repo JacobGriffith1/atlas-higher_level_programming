de48f156661f821d45c06e285eebc9a35a8fa115
JacobGriffith1 <jacobgriffith6339@gmail.com>
2024-06-19 02:30:58 -0500

    model_state [2.1]: trying to appease checker

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -6,7 +6,7 @@ an instance [Base = declarative_base()]
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 
-Base = declarative_base
+Base = declarative_base()
 
 
 class State(Base):
