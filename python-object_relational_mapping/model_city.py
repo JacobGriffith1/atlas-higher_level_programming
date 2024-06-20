87ee98660fda9d2faeb5f20fde375051987431ce
JacobGriffith1 <jacobgriffith6339@gmail.com>
2024-06-19 23:33:35 -0500

    14 [1]

--- python-object_relational_mapping/model_city.py
+++ python-object_relational_mapping/model_city.py
@@ -1,2 +1,22 @@
 #!/usr/bin/python3
+"""
+File contains the class definition of a [City]
+"""
+from model_state import Base
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.ext.declarative import declarative_base
 
+
+class City(Base):
+    """Class contains [id], [name], and [state_id] attributes of each [City]"""
+    __tablename__ = 'cities'
+
+    id = Column(Integer,
+                unique=True,
+                nullable=False,
+                primary_key=True)
+    name = Column(String(128),
+                  nullable=False)
+    state_id = Column(Integer,
+                      ForeignKey("states.id"),
+                      nullable=False)
