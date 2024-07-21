9c41150982995329654a7737bf02c5514fbc1c59
JacobGriffith1 <jacobgriffith6339@gmail.com>
2024-07-20 20:12:56 -0500

    3-value_argument [1]

--- javascript-warm_up/3-value_argument.js
+++ javascript-warm_up/3-value_argument.js
@@ -1,1 +1,7 @@
 #!/usr/bin/node
+// An undefined argv[2] indicates an abscence of passed arguments
+if (process.argv[2] === undefined) {
+  console.log('No argument');
+} else {
+  console.log(process.argv[2]);
+}
