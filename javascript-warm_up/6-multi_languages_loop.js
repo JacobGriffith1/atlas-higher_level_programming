d2c4ab08a6493a400a2ad0783c8676fff7b25377
JacobGriffith1 <jacobgriffith6339@gmail.com>
2024-07-20 20:44:13 -0500

    6-multi_languages_loop

--- javascript-warm_up/6-multi_languages_loop.js
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -1,1 +1,6 @@
 #!/usr/bin/node
+// JS can iterate through strings with a for loop, uses 'let' for conditions
+const text = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+for (let i = 0; i < 3; i++) {
+  console.log(text[i]);
+}
