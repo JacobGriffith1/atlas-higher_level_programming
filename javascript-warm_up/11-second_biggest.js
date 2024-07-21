1b14b2936918ce53cc2a6d79112e43727c7f15ee
JacobGriffith1 <jacobgriffith6339@gmail.com>
2024-07-20 22:51:22 -0500

    11-second_largest

--- javascript-warm_up/11-second_biggest.js
+++ javascript-warm_up/11-second_biggest.js
@@ -1,1 +1,19 @@
 #!/usr/bin/node
+let biggest = 0;
+let i;
+const nums = [];
+
+for (i = 2; i < process.argv.length; i++) {
+  if (Number.isNaN(parseInt(process.argv[i])) === false) {
+    nums[i - 2] = parseInt(process.argv[i]);
+  }
+}
+
+if (nums.length > 1) {
+  biggest = Math.max.apply(null, nums);
+  i = nums.indexOf(biggest);
+  nums[i] = -Infinity;
+  biggest = Math.max.apply(null, nums);
+}
+
+console.log(biggest);
