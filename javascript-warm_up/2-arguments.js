72ad36d7e3d154a1b86748faf0f7b4024a2b4499
JacobGriffith1 <jacobgriffith6339@gmail.com>
2024-07-20 20:02:41 -0500

    2-arguments [2]: SEMISTANDARD FIXING

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 // The first two arguments are process.execPath & the path to the JS file
 if (process.argv.length < 3) {
-    console.log('No argument');
+  console.log('No argument');
 } else if (process.argv.length === 3) {
-    console.log('Argument found');
+  console.log('Argument found');
 } else {
-    console.log('Arguments found');
+  console.log('Arguments found');
 }
