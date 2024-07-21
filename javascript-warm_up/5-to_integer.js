6f0d52341a97fd4a8454dda74391c6b7c4896d92
JacobGriffith1 <jacobgriffith6339@gmail.com>
2024-07-20 20:25:05 -0500

    5-to_integer

--- javascript-warm_up/5-to_integer.js
+++ javascript-warm_up/5-to_integer.js
@@ -1,1 +1,8 @@
 #!/usr/bin/node
+// parseInt converts a string to an int, NaN stands for 'Not a Number'
+const num = parseInt(process.argv[2]);
+if (Number.isNaN(num)) {
+  console.log('Not a number');
+} else {
+  console.log('My number: ' + num);
+}
