e2b54a7dd0c92928b6f96e8b14c52f4ec6dafc10
JacobGriffith1 <jacobgriffith6339@gmail.com>
2024-07-23 18:06:53 -0500

    6-square [1]

--- javascript_objects_scopes_closures/6-square.js
+++ javascript_objects_scopes_closures/6-square.js
@@ -1,1 +1,21 @@
 #!/usr/bin/node
+// Write a class Square that defines a square
+// and inherits from Square of 5-square.js:
+const SquareA = require('./5-square');
+
+class Square extends SquareA {
+  charPrint (c) {
+    if (c === undefined) {
+      c = 'X';
+    }
+    for (let i = 0; i < this.height; i++) {
+      let squ = '';
+      for (let j = 0; j < this.width; j++) {
+        squ += 'c';
+      }
+      console.log(squ);
+    }
+  }
+}
+
+module.exports = Square;
