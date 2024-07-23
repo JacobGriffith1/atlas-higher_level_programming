7e8655f15c7fd56502548a21cb248b59369d6218
JacobGriffith1 <jacobgriffith6339@gmail.com>
2024-07-23 17:18:40 -0500

    1-rec [1]

--- javascript_objects_scopes_closures/1-rectangle.js
+++ javascript_objects_scopes_closures/1-rectangle.js
@@ -1,1 +1,10 @@
 #!/usr/bin/node
+// Write a class Rectangle that defines a rectangle:
+class Rectangle {
+    constructor (w, h) {
+        this.w = width;
+        this.h = height;
+    }
+}
+
+module.exports = Rectangle;
