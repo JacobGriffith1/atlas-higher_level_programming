1a999d27c8593abcda207fe39240666909b7b501
JacobGriffith1 <jacobgriffith6339@gmail.com>
2024-07-23 18:31:23 -0500

    9-logme [1]

--- javascript_objects_scopes_closures/9-logme.js
+++ javascript_objects_scopes_closures/9-logme.js
@@ -1,1 +1,9 @@
 #!/usr/bin/node
+// Write a function that prints the number of arguments
+// already printed and the new argument value.
+let argc = 0;
+
+exports.logMe = function (item) {
+  console.log(argc + ': ' + item);
+  argc++;
+};
