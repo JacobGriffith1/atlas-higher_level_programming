e9c19a9e3d7633410ed95c19eecbf9b3703fc5c1
JacobGriffith1 <jacobgriffith6339@gmail.com>
2024-07-23 17:38:41 -0500

    3-rec [1]

--- javascript_objects_scopes_closures/3-rectangle.js
+++ javascript_objects_scopes_closures/3-rectangle.js
@@ -1,1 +1,22 @@
 #!/usr/bin/node
+// Write a class Rectangle that defines a rectangle:
+class Rectangle {
+  constructor (w, h) {
+    if ((w > 0) && (h > 0)) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      let rec = '';
+      for (let j = 0; j < this.width; j++) {
+        rec += 'X';
+      }
+      console.log(rec);
+    }
+  }
+}
+
+module.exports = Rectangle;
