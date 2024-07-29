fa60b5cbe87fc43743b594681dfd530ec8999b81
JacobGriffith1 <jacobgriffith6339@gmail.com>
2024-07-28 23:03:32 -0500

    0-readme [4]: Complete rebuild to see if anything changes. I really don't know why the checker is acting up.

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -1,11 +1,7 @@
 #!/usr/bin/node
-// File System module include
+
 const fs = require('fs');
-// Method readFile() is used to read files
-// Options for readFile() include
-// 1. What to read --- process.argv[2]
-// 2. How to read it --- 'utf8'
-// 3. What to do with it --- function (...)
+
 fs.readFile(process.argv[2], 'utf8', function (err, txt) {
   if (txt === undefined) {
     console.log(err);
