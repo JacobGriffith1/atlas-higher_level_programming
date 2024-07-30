1c3b8c6a90acdfb979a06db9c940dfda05d50c9e
JacobGriffith1 <jacobgriffith6339@gmail.com>
2024-07-30 01:22:02 -0500

    6-comtask [1]

--- javascript-web_scraping/6-completed_tasks.js
+++ javascript-web_scraping/6-completed_tasks.js
@@ -1,1 +1,18 @@
 #!/usr/bin/node
+const request = require('request');
+const url = process.argv[2];
+
+request(url, (err, res, body) => {
+  if (err) throw err;
+  const users = {};
+  for (const task of JSON.parse(body)) {
+    if (task.completed) {
+      if (!users[task.userId]) {
+        users[task.userId] = 1;
+      } else {
+        users[task.userId]++;
+      }
+    }
+  }
+  console.log(users);
+});
