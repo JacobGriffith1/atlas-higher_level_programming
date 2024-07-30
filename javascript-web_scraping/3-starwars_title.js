7610c8e66da45350bff0dd7cfc058bfae42fb9f1
JacobGriffith1 <jacobgriffith6339@gmail.com>
2024-07-30 00:20:19 -0500

    3-swtitle [4]: Took response back out, changed argv[]

--- javascript-web_scraping/3-starwars_title.js
+++ javascript-web_scraping/3-starwars_title.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const request = require('request');
-const id = process.argv[2];
-request('http://swapi.co/api/films/' + id + '/', function (error, response, body) {
+const id = process.argv[1];
+request('http://swapi.co/api/films/' + id + '/', function (error, body) {
   if (error == null) {
     const json = JSON.parse(body);
     console.log(json.title);
