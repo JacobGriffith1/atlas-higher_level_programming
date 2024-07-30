4fc158db52f775b09976ceaf34b43e87a40e99a6
JacobGriffith1 <jacobgriffith6339@gmail.com>
2024-07-30 00:52:06 -0500

    4-swcount [1]

--- javascript-web_scraping/4-starwars_count.js
+++ javascript-web_scraping/4-starwars_count.js
@@ -1,1 +1,18 @@
 #!/usr/bin/node
+const request = require('request');
+const id = 18;
+const url = process.argv[2];
+
+request(url + id, (err, res, body) => {
+  if (err) throw err;
+  const text = JSON.parse(body);
+  let i = 0;
+  for (const films of text.results) {
+    for (const people of films.people) {
+      if (people.includes(id)) {
+        i++;
+      }
+    }
+  }
+  console.log(i);
+});
