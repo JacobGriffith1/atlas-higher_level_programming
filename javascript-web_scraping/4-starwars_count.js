1e99ac17679033586fe4184cc2b0eb0a36f27e86
JacobGriffith1 <jacobgriffith6339@gmail.com>
2024-07-30 00:57:09 -0500

    4-swcount [2]: Modified request, it had some leftovers from the previous task that I had forgot to clean up.

--- javascript-web_scraping/4-starwars_count.js
+++ javascript-web_scraping/4-starwars_count.js
@@ -3,7 +3,7 @@ const request = require('request');
 const id = 18;
 const url = process.argv[2];
 
-request(url + id, (err, res, body) => {
+request(url, (err, res, body) => {
   if (err) throw err;
   const text = JSON.parse(body);
   let i = 0;
