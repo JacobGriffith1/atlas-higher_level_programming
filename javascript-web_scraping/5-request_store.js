67e420901f41a230f80dc3cc28b7508bdfecd27c
JacobGriffith1 <jacobgriffith6339@gmail.com>
2024-07-30 01:05:41 -0500

    5-reqstore [1]

--- javascript-web_scraping/5-request_store.js
+++ javascript-web_scraping/5-request_store.js
@@ -1,1 +1,12 @@
 #!/usr/bin/node
+const fs = require('fs');
+const request = require('request');
+const url = process.argv[2];
+const file = process.argv[3];
+
+request(url, (err, res, body) => {
+  if (err) throw err;
+  fs.writeFile(file, body, 'utf8', (err) => {
+    if (err) throw err;
+  });
+});
