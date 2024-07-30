a47b490a736784b43d9227c5324d758716563706
JacobGriffith1 <jacobgriffith6339@gmail.com>
2024-07-30 00:14:29 -0500

    3-swtitle [1]

--- javascript-web_scraping/3-starwars_title.js
+++ javascript-web_scraping/3-starwars_title.js
@@ -1,1 +1,10 @@
 #!/usr/bin/node
+const request = require('request');
+const id = process.argv[2];
+request('http://swapi.co/api/films/' + id + '/', function (error, response, body) {
+  if (error == null) {
+    console.log('Status Code:', response.statusCode);
+    const json = JSON.parse(body);
+    console.log(json.title);
+  }
+});
